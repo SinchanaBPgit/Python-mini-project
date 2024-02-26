dfe02ec41c56e7c2744e87eca86a460d0feb48c5
SinchanaBPgit <160091520+SinchanaBPgit@users.noreply.github.com>
2024-02-26 09:56:48 +0530

    Create main.py

---
+++ 10.Sudoku/main.py
@@ -0,0 +1,61 @@
+def print_board(board):
+    for row in board:
+        print(" ".join(str(cell) for cell in row))
+
+def is_valid(board, row, col, num):
+    if num in board[row] or num in [board[i][col] for i in range(9)]:
+        return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+def solve_sudoku(board):
+    empty_location = find_empty_location(board)
+
+    if not empty_location:
+        return True 
+    row, col = empty_location
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True 
+
+            board[row][col] = 0  
+    return False 
+
+
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Sudoku Puzzle:")
+print_board(sudoku_board)
+
+if solve_sudoku(sudoku_board):
+    print("\nSudoku Solution:")
+    print_board(sudoku_board)
+else:
+    print("\nNo solution exists.")
